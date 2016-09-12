91c46fb638ef78489a4a46737d94a69968156168
rincew1nd <xkorikx@gmail.com>
2016-09-12 20:55:46 +0300

    Initial commit

---
+++ UnitTests/AreaOfTriangleTest.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WorkTask;
+
+namespace UnitTests
+{
+	[TestFixture]
+	public class AreaOfTriangleTest
+	{
+		[Test]
+		public void Default()
+		{
+            Assert.AreEqual(Math.Round(Utils.AreaOfTriangle(5, 6, 3), 4), 7.4833);
+			Assert.AreEqual(Math.Round(Utils.AreaOfTriangle(5, 6, 10), 4), 11.399);
+			Assert.AreEqual(Math.Round(Utils.AreaOfTriangle(8, 6, 3), 4), 7.6444);
+		}
+
+		[Test]
+		public void NotRoundNumbers()
+		{
+			Assert.AreEqual(Math.Round(Utils.AreaOfTriangle(5.5, 6.1, 3.3), 4), 9.0217);
+		}
+
+		[Test]
+		public void ImpossibleTriangle()
+		{
+			Assert.Throws(typeof(ArgumentException), delegate { Utils.AreaOfTriangle(5, 6, 12); });
+			Assert.Throws(typeof(ArgumentException), delegate { Utils.AreaOfTriangle(5, 20, 10); });
+			Assert.Throws(typeof(ArgumentException), delegate { Utils.AreaOfTriangle(5, 3, 1); });
+		}
+
+		[Test]
+		public void NegativeNumbers()
+		{
+			Assert.AreEqual(Utils.AreaOfTriangle(-9, 6, 3), 0);
+			Assert.AreEqual(Utils.AreaOfTriangle(9, 6, -3), 0);
+		}
+	}
+}
---
+++ WorkTask/Utils.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkTask
+{
+    public static class Utils
+    {
+		/// <summary>
+		/// Calculate area of triangle by 3 sides
+		/// </summary>
+		/// <param name="a">Side 1</param>
+		/// <param name="b">Side 2</param>
+		/// <param name="c">Side 3</param>
+		/// <returns>Area of triangle</returns>
+		public static double AreaOfTriangle(double a, double b, double c)
+		{
+			var sA = new List<double>() { a, b, c }
+				.Select(e => { e = Math.Abs(e); return e; })
+				.OrderBy(e => e)
+				.ToList();
+
+			if (sA[2] > sA[0] + sA[1])
+				throw new ArgumentException("Impossible triangle. One side larger then sum of others.");
+
+			var halfArea = (a + b + c) / 2;
+			return Math.Sqrt(halfArea * (halfArea - sA[0]) * (halfArea - sA[1]) * (halfArea - sA[2]));
+		}
+    }
+}
